da298b6f35c8832fe6af56e74e70599fdb575bf5
IT24101714 <177148529+IT24101714@users.noreply.github.com>
2025-10-14 13:31:18 +0530

    Add files via upload

---
+++ Lab Sheet 10/Lab 10 PS - IT24101714.R
@@ -0,0 +1,27 @@
+setwd("C:\Users\IT24101714\Desktop\IT24101714\LAB_10")
+
+#1
+#i
+observed <- c(55, 62, 43, 46, 50)
+prob <- c(.2, .2, .2, .2, .2)
+chisq.test(x=observed, p=prob)
+
+#2
+#i
+file_path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
+
+housetasks <- read.delim(file_path, row.names = 1)
+housetasks
+
+#ii
+chisq <- chisq.test(housetasks)
+chisq
+
+#EXERCISE
+#1
+#ii
+observed_counts <- c(120, 95, 85, 100)
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+chi_test_result <- chisq.test(x = observed_counts, p = probabilities)
+print(chi_test_result)
+
